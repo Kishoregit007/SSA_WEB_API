ebcfed487755fe431d2c7728f8422dc396322e17
Kishore Patra <career.kishorepatra@gmail.com>
2022-02-22 01:22:03 +0530

    initial commit

---
+++ src/main/java/in/ashokit/Jrtp19SsaWebApiApplication.java
@@ -0,0 +1,13 @@
+package in.ashokit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jrtp19SsaWebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jrtp19SsaWebApiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/in/ashokit/service/SsaService.java
@@ -0,0 +1,22 @@
+package in.ashokit.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SsaService {
+    public String fetchStateName(String ssn){
+        String stateName = null;
+        if (ssn.startsWith("4")){
+            stateName = "New Jersey";
+        }else if (ssn.startsWith("5")){
+            stateName = "Rhode Island";
+        }else if (ssn.startsWith("6")){
+            stateName = "Kentucky";
+        }else if (ssn.startsWith("7")){
+            stateName = "Ohio";
+        }else{
+            stateName = "Invalid statename";
+        }
+        return stateName;
+    }
+}
